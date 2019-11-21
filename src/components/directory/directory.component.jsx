8e58ad011867360337f8eee55dcf867db911e6d0
bilal898 <bfhamid@gmail.com>
2019-11-21 09:05:14 +0500

    midway lesson 62 8.01

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import sections from './directory.data'
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss'
+
+export default class Directory extends Component {
+
+    state = {
+        sections: sections
+    }
+    render() {
+        return (
+            <div className="directory-menu">
+                {this.state.sections.map(section => {
+                    return (
+                        <MenuItem 
+                        title={section.title} 
+                        key={section.id}
+                        imageUrl={section.imageUrl}
+                        />
+                    )
+                })}
+            </div>
+        )
+    }
+}
